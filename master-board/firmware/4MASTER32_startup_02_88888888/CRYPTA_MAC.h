f2a039d918210cdca60b0cbabad6ab1b63a1e573
Luciano Pautasso <lpautas@gmail.com>
2026-08-03 19:15:59 +0200

    Delete MAC protection constants
    Removed MAC protection constants from CRYPTA_MAC.h

--- master-board/firmware/4MASTER32_startup_02_88888888/CRYPTA_MAC.h
+++ master-board/firmware/4MASTER32_startup_02_88888888/CRYPTA_MAC.h
@@ -7,10 +7,6 @@
 // Header generato per la protezione MAC del firmware master.
 // I valori qui sotto sono derivati dal MAC autorizzato.
 
-const int p_interval = 7370;
-const long p_timestamp = 54308;
-const double p_drift = 177.00;
-const char* p_token = "67";
 
 
 
